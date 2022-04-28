49d6bbeee843b9d5665cb9d173fb2d6240de15ad
Harim <harimz@Harims-MacBook-Pro.local>
2022-04-28 13:40:12 -0700

    Initial setup

---
+++ components/layout/navigation.js
@@ -0,0 +1,8 @@
+import { Box, Container } from "@mui/material";
+import React from "react";
+
+const Navigation = () => {
+  return <Container maxWidth="lg" sx={{ border: "2px solid red" }}></Container>;
+};
+
+export default Navigation;
